a3bae7fe50e3acc4fba015381addbc0e4b1bff7c
Costa Huang <costa.huang@outlook.com>
2023-01-10 11:30:17 -0500

    refactor

--- cleanrlhf/model.py
+++ cleanrlhf/model.py
@@ -51,21 +51,21 @@ class CausalSelfAttention(nn.Module):
     def __call__(self, x: jnp.array, attn_pdrop_key: jax.random.KeyArray, resid_pdrop_key: jax.random.KeyArray):
         assert self.embd_dim % self.n_head == 0, "embd_dim must be divisible by num_heads"
         B, T, C = jnp.shape(x)  # batch size, sequence length, embedding dimensionality (embd_dim
-        head_dim = C // self.n_head  # alias: hm
+        head_dim = C // self.n_head  # alias: hd
         bias = jnp.tril(jnp.ones((self.block_size, self.block_size))).reshape(1, 1, self.block_size, self.block_size)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
         c_attn = nn.Dense(3 * C)(x)  # (B, T, 3 * C), `c_attn` means `concatenated attention`
         q, k, v = jnp.split(c_attn, 3, axis=-1)  # each has shape (B, T, C)
-        q = q.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hm), nh: n_head, hm: head dimensionality
-        k = k.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hm)
-        v = v.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hm)
+        q = q.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hd), nh: n_head, hd: head dimensionality
+        k = k.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hd)
+        v = v.reshape(B, T, self.n_head, head_dim).swapaxes(1, 2)  # (B, nh, T, hd)
         attn = q @ k.transpose(0, 1, 3, 2) / jnp.sqrt(head_dim)  # (B, nh, T, T), attention scores
         attn = jnp.where(bias == 0, float("-inf"), attn)  # (B, nh, T, T), mask out the future tokens
         attn = nn.softmax(attn, axis=-1)  # (B, nh, T, T), attention weights (probabilities)
         attn = dropout(attn, rate=self.attn_pdrop, key=attn_pdrop_key)
-        y = attn @ v  # (B, nh, T, hm)
-        y = y.swapaxes(1, 2)  # (B, T, nh, hm)
+        y = attn @ v  # (B, nh, T, hd)
+        y = y.swapaxes(1, 2)  # (B, T, nh, hd)
         y = y.reshape(B, T, C)  # (B, T, C)
         y = nn.Dense(C)(y)  # (B, T, C)
         y = dropout(y, rate=self.resid_pdrop, key=resid_pdrop_key)  # (B, T, C)
@@ -99,67 +99,55 @@ class Block(nn.Module):
         x = oldx + x
         return x
 
-    def forward(self, x):
-        x = x + self.attn(self.ln_1(x))
-        x = x + self.mlpf(self.ln_2(x))
-        return x
-
-
-class GPT(nn.Module):
-    n_layer: int
-    n_head: int
-    embd_dim: int
-    # these options must be filled in externally
-    vocab_size: int
-    block_size: int
+@dataclass
+class GPTConfig:
+    n_layer: int = 3
+    n_head: int = 3
+    embd_dim: int = 48
     # dropout hyperparameters
     embd_pdrop: int = 0.1
     resid_pdrop: int = 0.1
     attn_pdrop: int = 0.1
 
-    # self.transformer = nn.ModuleDict(dict(
-    #     wte = nn.Embed(self.vocab_size, self.embd_dim), # word to embedding
-    #     wpe = nn.Embed(self.block_size, self.embd_dim), # word to position embedding
-    #     drop = nn.Dropout(self.embd_pdrop),
-    #     h = nn.ModuleList([Block(self) for _ in range(self.n_layer)]),
-    #     ln_f = nn.LayerNorm(self.embd_dim),
-    # ))
-    # self.lm_head = nn.Linear(self.embd_dim, self.vocab_size, bias=False)
+
+class GPT(nn.Module):
+    c: GPTConfig
+
+    # these options must be filled in externally
+    vocab_size: int = None
+    block_size: int = None
+
+    # TODO: implement layer init, optimizer configurations stuff
+    # https://github.com/karpathy/minGPT/blob/37baab71b9abea1b76ab957409a1cc2fbfba8a26/mingpt/model.py#L163-L172
+    # https://github.com/karpathy/minGPT/blob/37baab71b9abea1b76ab957409a1cc2fbfba8a26/mingpt/model.py#L215-L258
 
     @nn.compact
-    def __call__(self, idx: jnp.array, target: jnp.array, key: jax.random.KeyArray):
+    def __call__(self, idx: jnp.array, targets: jnp.array, key: jax.random.KeyArray):
         key, embd_pdrop_key = jax.random.split(key, 2)
         _, T = jnp.shape(idx)  # B, T
         assert T <= self.block_size, f"Cannot forward sequence of length {T}, block size is only {self.block_size}"
         pos = jnp.array([jnp.arange(0, T, dtype=jnp.int32)])  # shape (1, T)
 
         # forward the GPT model itself
-        tok_embd = nn.Embed(self.vocab_size, self.embd_dim)(idx)  # token embeddings of shape (B, T, embd_dim)
-        pos_embd = nn.Embed(self.block_size, self.embd_dim)(pos)  # position embeddings of shape (1, T, embd_dim)
-        x = dropout(tok_embd + pos_embd, rate=self.embd_pdrop, key=embd_pdrop_key)
-        for _ in range(self.n_layer):
+        tok_embd = nn.Embed(self.vocab_size, self.c.embd_dim)(idx)  # token embeddings of shape (B, T, embd_dim)
+        pos_embd = nn.Embed(self.block_size, self.c.embd_dim)(pos)  # position embeddings of shape (1, T, embd_dim)
+        x = dropout(tok_embd + pos_embd, rate=self.c.embd_pdrop, key=embd_pdrop_key)
+        for _ in range(self.c.n_layer):
             x = Block(
-                self.embd_dim,
-                self.n_head,
-                self.attn_pdrop,
-                self.resid_pdrop,
+                self.c.embd_dim,
+                self.c.n_head,
+                self.c.attn_pdrop,
+                self.c.resid_pdrop,
                 self.block_size,
             )(x, key)
-        x = nn.LayerNorm(self.embd_dim)(x)
+        x = nn.LayerNorm(self.c.embd_dim)(x)
         logits = nn.Dense(self.vocab_size, use_bias=False)(x)
 
         # Costa: mask out the gradient for indices with values = -1
         # should be equivalent to `ignore_index=-1` in F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
-        logits = jnp.where(target == -1, -1e8, logits)
-        loss = optax.softmax_cross_entropy(logits.reshape(-1, jnp.shape(logits)[-1]), target.reshape(-1))
+        logits = jnp.where(jnp.expand_dims(targets, axis=-1) == -1, jnp.zeros_like(logits) -1e8 , logits)
+        loss = optax.softmax_cross_entropy_with_integer_labels(logits.reshape(-1, jnp.shape(logits)[-1]), targets.reshape(-1))
         return loss, logits
-
-
-@dataclass
-class GPTConfig:
-    n_layer: int
-    n_head: int
-    embd_dim: int
 
 
 MODELS_PRESET: Dict[str, GPTConfig] = {
@@ -204,13 +192,17 @@ if __name__ == "__main__":
     # GPT Demo
     n_layer = 3
     vocab_size = 10
+    
     gpt = GPT(
-        n_layer=n_layer,
-        n_head=n_head,
-        embd_dim=embd_dim,
+        c=GPTConfig(
+            n_layer=n_layer,
+            n_head=n_head,
+            embd_dim=embd_dim,
+        ),
         vocab_size=vocab_size,
         block_size=block_size,
     )
     x = jax.random.randint(key, (1, block_size), minval=0, maxval=vocab_size)  # B, T; or batch_size, sequence_length
+    y = jax.random.randint(key, (1,), minval=0, maxval=vocab_size)  # B; or batch_size, sequence_length
     gpt_params = gpt.init(params_key, x, x, key)
     gpt_y = gpt.apply(gpt_params, x, x, key)
